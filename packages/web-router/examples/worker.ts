962820bcf18efd56c346b03b966cc7f04b6fd620
Jeremy Danyow <jdanyow@gmail.com>
2020-01-03 09:34:59 -0800

    chore(all): add homepage attribute
    improve router example

--- packages/web-router/examples/worker.ts
+++ packages/web-router/examples/worker.ts
@@ -1,35 +1,57 @@
 import { Application } from '@cfworker/web';
 import { Router } from '@cfworker/web-router';
 
+const database = new Map<string, string>();
+database.set('default-message', 'Greetings earthling.');
+database.set('hello-world', 'Hello world!');
+
 const router = new Router();
 
-let message = 'Hello world!';
-
 router
-  .get('/', ({ res }) => {
+  .get('/:id?', ({ res, state }) => {
+    const id = state.params.id;
+    if (!id) {
+      res.redirect('/default-message');
+      return;
+    }
+    const message = database.get(id) || '';
+
     res.headers.set('content-type', 'text/html');
-    // Caution: message should be escaped to avoid an HTML injection vulnerability.
     res.body = `
-      <h1>${message} at ${new Date().toLocaleTimeString()}</h1>
-      <form action="/api/message" method="POST">
+      <h1>${new Date().toLocaleTimeString()}: "${message}"</h1>
+      <form action="/api/messages/${id}" method="POST">
         <label>
             Message
             <input name="message" value="${message}">
         </label>
         <button type="submit">Submit</button>
       </form>
-      <p><a href="/api/message"><code>/api/message</code></a></p>`;
+      <p><a href="/api/messages/${id}">/api/messages/${id}</a></p>
+      <p><a href="/hello-world">/hello-world</a></p>`;
   })
-  .get('/api/message', ({ res }) => {
-    res.headers.set('content-type', 'application/json');
-    res.body = JSON.stringify({ message });
+  .get('/api/messages/:id', ({ res, state }) => {
+    const id = state.params.id;
+    if (database.has(id)) {
+      const message = database.get(id);
+      res.headers.set('content-type', 'application/json');
+      res.body = JSON.stringify({ message });
+    } else {
+      res.status = 404;
+      res.body = `Message with id "${id}" does not exist.`;
+    }
   })
-  .post('/api/message', async ({ req, res }) => {
+  .post('/api/messages/:id', async ({ req, res, state }) => {
+    const id = state.params.id;
     const data = await req.formData();
-    message = data.get('message').toString();
-    res.redirect('/');
+    const message = data.get('message').toString();
+    database.set(id, message);
+    res.redirect(`/${id}`);
   });
 
 const app = new Application();
 app.use(router.middleware);
 app.listen();
+
+/*
+yarn workspace @cfworker/web-router cfworker run examples/worker.ts --watch --nocheck
+*/
