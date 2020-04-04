50d6d5546f579439b5eae762257ab3b28dd40a70
Jeremy Danyow <jdanyow@gmail.com>
2020-04-04 15:01:24 -0700

    test(jwt): add decode and parse

--- packages/jwt/src/verify.ts
+++ packages/jwt/src/verify.ts
@@ -1,40 +1,16 @@
-import { DecodedJwt, JsonWebKeyset } from './types';
+import { DecodedJwt } from './types';
+import { getkey } from './jwks';
 
 /**
- * Verify
- * TODO: refactor this to preserve reference to verification function.
- * No need to import the key on each signature verification.
+ * Verify the JWT's signature.
  * @param {DecodedJwt} decoded
  */
-export async function verifyJwtSignature(
-  decoded: DecodedJwt,
-  jwks: JsonWebKeyset
-) {
+export async function verifyJwtSignature(decoded: DecodedJwt) {
   const encoder = new TextEncoder();
   const data = encoder.encode(`${decoded.raw.header}.${decoded.raw.payload}`);
   const signature = new Uint8Array(
     Array.from(decoded.signature).map(c => c.charCodeAt(0))
   );
-  const {
-    keys: [{ x5c, n, kid, x5t }]
-  } = jwks;
-  const jwk = {
-    alg: 'RS256',
-    kty: 'RSA',
-    key_ops: ['verify'],
-    use: 'sig',
-    x5c,
-    n,
-    e: 'AQAB',
-    kid,
-    x5t
-  };
-  const key = await crypto.subtle.importKey(
-    'jwk',
-    jwk,
-    { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
-    false,
-    ['verify']
-  );
+  const key = await getkey(decoded);
   return crypto.subtle.verify('RSASSA-PKCS1-v1_5', key, signature, data);
 }
---
+++ packages/jwt/test/parse.spec.ts
@@ -0,0 +1,106 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { parseJwt } from '../src/parse';
+import { encode } from '@cfworker/base64url';
+import { importKey } from '../src/jwks';
+import { JwtHeader, JwtPayload } from '../src/types';
+
+const iss = 'https://test.com';
+const aud = 'test-aud';
+const sub = 'test-sub';
+const iat = Math.floor(new Date().getTime() / 1000);
+
+describe('parseJwt', () => {
+  it('parses a valid JWT', async () => {
+    const exp = Math.floor(new Date().getTime() / 1000) + 10;
+    const header: JwtHeader = { alg: 'RS256', typ: 'JWT', kid: 'xyz' };
+    const payload = { iss, aud, exp, sub, iat };
+    const jwt = await createJwt(header, payload);
+    const result = await parseJwt(jwt, iss, aud);
+    expect(result.valid).to.equal(true);
+  });
+
+  it('rejects unexpected algorithm', async () => {
+    const exp = Math.floor(new Date().getTime() / 1000) + 10;
+    const header: JwtHeader = { alg: 'HS256', typ: 'JWT', kid: 'xyz' };
+    const payload = { iss, aud, exp, sub, iat };
+    const jwt = await createJwt(header, payload);
+    const result = await parseJwt(jwt, iss, aud);
+    expect(result.valid).to.equal(false);
+  });
+
+  it('rejects unexpected type', async () => {
+    const exp = Math.floor(new Date().getTime() / 1000) + 10;
+    const header: JwtHeader = { alg: 'RS256', typ: 'JWS', kid: 'xyz' };
+    const payload = { iss, aud, exp, sub, iat };
+    const jwt = await createJwt(header, payload);
+    const result = await parseJwt(jwt, iss, aud);
+    expect(result.valid).to.equal(false);
+  });
+
+  it('rejects invalid issuer', async () => {
+    const exp = Math.floor(new Date().getTime() / 1000) + 60;
+    const header: JwtHeader = { alg: 'RS256', typ: 'JWT', kid: 'xyz' };
+    const payload = { iss: 'https://nefarious.com', aud, exp, sub, iat };
+    const jwt = await createJwt(header, payload);
+    const result = await parseJwt(jwt, iss, aud);
+    expect(result.valid).to.equal(false);
+  });
+
+  it('rejects invalid audience', async () => {
+    const exp = Math.floor(new Date().getTime() / 1000) + 60;
+    const header: JwtHeader = { alg: 'RS256', typ: 'JWT', kid: 'xyz' };
+    const payload = { iss, aud: 'nefarious', exp, sub, iat };
+    const jwt = await createJwt(header, payload);
+    const result = await parseJwt(jwt, iss, aud);
+    expect(result.valid).to.equal(false);
+  });
+
+  it('rejects expired JWT', async () => {
+    const exp = Math.floor(new Date().getTime() / 1000) - 60;
+    const header: JwtHeader = { alg: 'RS256', typ: 'JWT', kid: 'xyz' };
+    const payload = { iss, aud, exp, sub, iat };
+    const jwt = await createJwt(header, payload);
+    const result = await parseJwt(jwt, iss, aud);
+    expect(result.valid).to.equal(false);
+  });
+});
+
+let privateKey: CryptoKey;
+
+async function createJwt(
+  header: JwtHeader,
+  payload: JwtPayload
+): Promise<string> {
+  if (!privateKey) {
+    privateKey = await generateKey();
+  }
+
+  const data =
+    encode(JSON.stringify(header)) + '.' + encode(JSON.stringify(payload));
+
+  const signature = await crypto.subtle.sign(
+    'RSASSA-PKCS1-v1_5',
+    privateKey,
+    new TextEncoder().encode(data)
+  );
+
+  return data + '.' + btoa(String.fromCharCode(...new Uint8Array(signature)));
+}
+
+async function generateKey() {
+  const keyPair = await crypto.subtle.generateKey(
+    {
+      name: 'RSASSA-PKCS1-v1_5',
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: { name: 'SHA-256' }
+    },
+    true,
+    ['sign', 'verify']
+  );
+
+  const jwk = await crypto.subtle.exportKey('jwk', keyPair.publicKey);
+  await importKey(iss, jwk);
+  return keyPair.privateKey;
+}
