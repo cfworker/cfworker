e2e483daaad41d9ccde189c4978c89a41226ae64
Johan Bjork <johan.bjork@twosigmaiq.com>
2020-07-22 15:07:05 +1000

    fix(json-schema): add option to report all errors (#63)

--- packages/json-schema/test/validator.spec.ts
+++ packages/json-schema/test/validator.spec.ts
@@ -33,4 +33,56 @@ describe('Validator', () => {
     expect(validator.validate(true).valid).to.equal(true);
     expect(validator.validate('hello world').valid).to.equal(false);
   });
+
+  it('validate all array entries with nested errors', () => {
+    const validator = new Validator(
+      {
+        type: 'array',
+        items: {
+          name: { type: 'string' },
+          email: { type: 'string' },
+          required: ['name', 'email']
+        }
+      },
+      '2019-09',
+      false
+    );
+
+    const res = validator.validate([
+      {
+        name: 'hello'
+        //missing email
+      },
+      {
+        //missing name
+        email: 'a@b.c'
+      }
+    ]);
+    expect(res.valid).to.equal(false);
+    expect(res.errors.length).to.equal(4);
+  });
+
+  it('validate all object properties with nested errors', () => {
+    const validator = new Validator(
+      {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          email: { type: 'string' },
+          number: { type: 'number' },
+          required: ['name', 'email', 'number']
+        }
+      },
+      '2019-09',
+      false
+    );
+
+    const res = validator.validate({
+      name: 'hello',
+      email: 5, //invalid type
+      number: 'Hello' //invalid type
+    });
+    expect(res.valid).to.equal(false);
+    expect(res.errors.length).to.equal(4);
+  });
 });
