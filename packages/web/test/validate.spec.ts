f1146ca4ea105b87805523a082ee685fca00ace4
Jeremy Danyow <jdanyow@gmail.com>
2020-11-29 12:27:58 -0800

    fix(web): validate multi-part form data

--- packages/web/test/validate.spec.ts
+++ packages/web/test/validate.spec.ts
@@ -86,22 +86,97 @@ describe('validate', () => {
     expect(resolved).to.be.false;
   });
 
-  it('validates body', async () => {
-    const request = new Request('https://a.b/items/899934?category=recent', {
+  it('validates JSON body', async () => {
+    const middleware = validate({ body: { required: ['id'] } });
+    let request = new Request('https://a.b/', {
       method: 'POST',
       body: JSON.stringify({}),
       headers: {
         'content-type': 'application/json'
       }
     });
-    const context = new Context({ request });
-    context.req.params.id = '899934';
+    let context = new Context({ request });
     let resolved = false;
     try {
       await middleware(context, async () => (resolved = true));
     } catch (err) {
       expect(err instanceof HttpError && err.status === 400).to.be.true;
     }
+    // verify json read
+    await context.req.body.json();
     expect(resolved).to.be.false;
+
+    request = new Request('https://a.b/', {
+      method: 'POST',
+      body: JSON.stringify({ id: 'world' }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+    context = new Context({ request });
+    await middleware(context, async () => (resolved = true));
+    // verify json read
+    await context.req.body.json();
+    expect(resolved).to.be.true;
+  });
+
+  it('validates URL encoded body', async () => {
+    const middleware = validate({ body: { required: ['id'] } });
+    let request = new Request('https://a.b/', {
+      method: 'POST',
+      body: new URLSearchParams({ hello: 'world' })
+    });
+    let context = new Context({ request });
+    let resolved = false;
+    try {
+      await middleware(context, async () => (resolved = true));
+    } catch (err) {
+      expect(err instanceof HttpError && err.status === 400).to.be.true;
+    }
+    // verify formData read
+    await context.req.body.formData();
+    expect(resolved).to.be.false;
+
+    request = new Request('https://a.b/', {
+      method: 'POST',
+      body: new URLSearchParams({ id: 'world' })
+    });
+    context = new Context({ request });
+    await middleware(context, async () => (resolved = true));
+    // verify formData read
+    await context.req.body.formData();
+    expect(resolved).to.be.true;
+  });
+
+  it('validates multi-part form encoded body', async () => {
+    const middleware = validate({ body: { required: ['id'] } });
+    let body = new FormData();
+    body.set('hello', 'world');
+    let request = new Request('https://a.b/', {
+      method: 'POST',
+      body
+    });
+    let context = new Context({ request });
+    let resolved = false;
+    try {
+      await middleware(context, async () => (resolved = true));
+    } catch (err) {
+      expect(err instanceof HttpError && err.status === 400).to.be.true;
+    }
+    // verify formData read
+    await context.req.body.formData();
+    expect(resolved).to.be.false;
+
+    body = new FormData();
+    body.set('id', 'world');
+    request = new Request('https://a.b/', {
+      method: 'POST',
+      body
+    });
+    context = new Context({ request });
+    await middleware(context, async () => (resolved = true));
+    // verify formData read
+    await context.req.body.formData();
+    expect(resolved).to.be.true;
   });
 });
