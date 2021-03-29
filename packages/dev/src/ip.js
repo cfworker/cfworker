4a7bb3b70f5501a6cbe8a5ea294e6d57f730c6ea
Florian Klampfer <mail@qwtel.com>
2021-03-29 11:51:36 +0700

    Quietly fail when geojs is blocked

--- packages/dev/src/ip.js
+++ packages/dev/src/ip.js
@@ -4,10 +4,14 @@ import fetch from 'node-fetch';
  * @returns {Promise<string|null>}
  */
 export async function getLocalhostIP() {
-  const response = await fetch('https://get.geojs.io/v1/ip.json');
-  if (!response.ok) {
+  try {
+    const response = await fetch('https://get.geojs.io/v1/ip.json');
+    if (!response.ok) {
+      return null;
+    }
+    const data = await response.json();
+    return data.ip;
+  } catch {
     return null;
   }
-  const data = await response.json();
-  return data.ip;
 }
