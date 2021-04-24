88d6b73514a950d2b1157d4b8129a385e01e0968
Jeremy Danyow <jdanyow@gmail.com>
2021-04-24 16:26:57 -0700

    decouple key retrieval from signature verification

--- packages/jwt/src/verify.ts
+++ packages/jwt/src/verify.ts
@@ -1,16 +1,15 @@
-import { getKey } from './jwks.js';
 import { DecodedJwt } from './types.js';
 
 /**
  * Verify the JWT's signature.
  * @param {DecodedJwt} decoded
  */
-export async function verifyJwtSignature(decoded: DecodedJwt) {
+export async function verifyJwtSignature(decoded: DecodedJwt, key: CryptoKey) {
   const encoder = new TextEncoder();
   const data = encoder.encode(`${decoded.raw.header}.${decoded.raw.payload}`);
   const signature = new Uint8Array(
     Array.from(decoded.signature).map(c => c.charCodeAt(0))
   );
-  const key = await getKey(decoded);
+
   return crypto.subtle.verify('RSASSA-PKCS1-v1_5', key, signature, data);
 }
