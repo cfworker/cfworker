21840b867fc87954567cd35ed61581a59aab8a97
Jeremy Danyow <jdanyow@gmail.com>
2021-08-29 18:24:55 -0700

    unevaluated fixes

--- packages/json-schema/src/index.ts
+++ packages/json-schema/src/index.ts
@@ -1,6 +1,5 @@
 export * from './deep-compare-strict.js';
 export * from './dereference.js';
-export * from './evaluated.js';
 export * from './format.js';
 export * from './pointer.js';
 export * from './types.js';
