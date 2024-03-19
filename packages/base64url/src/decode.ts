d5f0e6b657cd014788736e9c5d7958f3a40866e3
Jeremy Danyow <jdanyow@gmail.com>
2024-03-19 07:22:55 -0400

    remove deprecated packages

--- packages/base64url/src/decode.ts
+++
@@ -1,9 +0,0 @@
-import { base64url } from 'rfc4648';
-
-/**
- * Base64 URL decode a string.
- * @deprecated Use rfc4648 package directly
- */
-export function decode(s: string): string {
-  return new TextDecoder().decode(base64url.parse(s, { loose: true }));
-}
