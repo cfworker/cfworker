582c83be026acfa3886455eaa58d76bc468d877a
Jeremy Danyow <jdanyow@gmail.com>
2024-08-21 18:16:32 -0700

    fix export of InvalidJwtReasonCode

--- packages/jwt/src/index.ts
+++ packages/jwt/src/index.ts
@@ -2,4 +2,5 @@ export { decodeJwt } from './decode.js';
 export { getJwks, getKey, importKey } from './jwks.js';
 export { parseJwt } from './parse.js';
 export * from './types.js';
+export { InvalidJwtReasonCode } from './types.js';
 export { verifyJwtSignature } from './verify.js';
