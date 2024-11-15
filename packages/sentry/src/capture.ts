58c1bad1c2a4f3d2b6983cb1f206a07f2b1aff1d
Jeremy Danyow <jdanyow@gmail.com>
2024-11-15 09:50:42 -0800

    improve error serialization

--- packages/sentry/src/capture.ts
+++ packages/sentry/src/capture.ts
@@ -1,6 +1,82 @@
-import { serializeError } from './tracekit.js';
-
 const sentryClient = 'cfworker-sentry';
+
+const UNKNOWN_FUNCTION = '?';
+const FILENAME_MATCH = /^\s*[-]{4,}$/;
+const FULL_MATCH =
+  /at (?:async )?(?:(.+?)\s+\()?(?:(.+):(\d+):(\d+)?|([^)]+))\)?/;
+
+function parseLine(line: string) {
+  const lineMatch = line.match(FULL_MATCH);
+
+  if (lineMatch) {
+    let object: string | undefined;
+    let method: string | undefined;
+    let functionName: string | undefined;
+    let typeName: string | undefined;
+    let methodName: string | undefined;
+
+    if (lineMatch[1]) {
+      functionName = lineMatch[1];
+
+      let methodStart = functionName.lastIndexOf('.');
+      if (functionName[methodStart - 1] === '.') {
+        methodStart--;
+      }
+
+      if (methodStart > 0) {
+        object = functionName.slice(0, methodStart);
+        method = functionName.slice(methodStart + 1);
+        const objectEnd = object.indexOf('.Module');
+        if (objectEnd > 0) {
+          functionName = functionName.slice(objectEnd + 1);
+          object = object.slice(0, objectEnd);
+        }
+      }
+      typeName = undefined;
+    }
+
+    if (method) {
+      typeName = object;
+      methodName = method;
+    }
+
+    if (method === '<anonymous>') {
+      methodName = undefined;
+      functionName = undefined;
+    }
+
+    if (functionName === undefined) {
+      methodName = methodName || UNKNOWN_FUNCTION;
+      functionName = typeName ? `${typeName}.${methodName}` : methodName;
+    }
+
+    let filename = './' + lineMatch[2]?.split('/').pop();
+    const isNative = lineMatch[5] === 'native';
+    if (!filename && lineMatch[5] && !isNative) {
+      filename = './' + lineMatch[5]?.split('/').pop();
+    }
+
+    return {
+      filename,
+      function: functionName,
+      lineno: _parseIntOrUndefined(lineMatch[3]),
+      colno: _parseIntOrUndefined(lineMatch[4]),
+      in_app: true
+    };
+  }
+
+  if (line.match(FILENAME_MATCH)) {
+    return {
+      filename: line
+    };
+  }
+
+  return undefined;
+}
+
+function _parseIntOrUndefined(input: string | undefined): number | undefined {
+  return parseInt(input || '', 10) || undefined;
+}
 
 /**
  * Report an error to Sentry
@@ -17,18 +93,20 @@ export function captureError({
   sentryDsn: string;
   environment: string;
   release: string;
-  err: any;
+  err: unknown;
   request: Request;
   user: any;
   level?: 'fatal' | 'error' | 'warning' | 'info' | 'debug';
 }): { event_id: string; posted: Promise<Response> } {
   const event_id = crypto.randomUUID();
   const timestamp = new Date().toISOString().substr(0, 19);
-  if (!(err instanceof Error)) {
-    err = Object.prototype.valueOf.call(err ?? {});
-    Error.captureStackTrace(err, captureError);
+  let error: Error;
+  if (err instanceof Error) {
+    error = err;
+  } else {
+    error = new Error(err as string);
+    error.message = String(err);
   }
-  const parsed = serializeError(err);
   const body = {
     event_id,
     timestamp,
@@ -43,22 +121,20 @@ export function captureError({
     // tags: {},
     environment,
     // modules: {},
-    extra: {
-      toString: err.toString(),
-      stack: err.stack
-    },
+    extra: {},
     exception: {
       values: [
         {
-          type: parsed.name,
-          value: parsed.message,
+          type: error.name,
+          value: error.message,
+          // Ref: https://develop.sentry.dev/sdk/event-payloads/stacktrace
           stacktrace: {
-            frames: parsed.stack.reverse().map(s => ({
-              filename: s.url,
-              function: s.func,
-              lineno: s.line,
-              colno: s.column
-            }))
+            frames:
+              error.stack
+                ?.split('\n')
+                .map(parseLine)
+                .filter(Boolean)
+                .reverse() ?? []
           }
         }
       ]
