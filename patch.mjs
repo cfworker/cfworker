04a079e21addc08a5278bf61166862305b58e7a5
Jeremy Danyow <jdanyow@gmail.com>
2024-12-10 13:28:01 -0800

    update dependencies

--- patch.mjs
+++
@@ -1,6 +0,0 @@
-// https://github.com/swansontec/rfc4648.js/pull/36
-import { readFile, writeFile } from 'fs/promises';
-const filename = `${import.meta.dirname}/node_modules/rfc4648/lib/src/index.d.ts`;
-const original = await readFile(filename, { encoding: 'utf8' });
-const patched = original.replace(`from './codec';`, `from './codec.js';`);
-await writeFile(filename, patched);
